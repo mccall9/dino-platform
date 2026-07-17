42201d928b064d3af883a060add8768f7ec008ea
dino <dino@local>
2026-07-17 17:53:18 -0300

    Use Arvo as the global UI typeface

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -27,6 +27,19 @@ export const Route = createRootRoute({
       }),
     ],
     links: [
+      {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossOrigin: "anonymous",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&display=swap",
+      },
       { rel: "stylesheet", href: appCss },
       { rel: "icon", href: "/favicon.ico" },
     ],
